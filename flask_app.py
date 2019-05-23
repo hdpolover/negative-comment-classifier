06072472051fdef6de425797ef6272aa082f8198
Nurami Nasrullah <45956886+nasrullah1@users.noreply.github.com>
2019-05-23 23:19:53 +0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,43 @@
 from flask import Flask, render_template, url_for, request, redirect
 
-app = Flask(__name__)
+#import classifier.py
+from classifier import pred
+
+app = Flask(_name_)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
-if __name__ == '__main__':
+@app.route("/clf")
+def clf():
+    return render_template("classifier.html")
+
+@app.route("/predi", methods=['POST'])
+def predi():
+    if request.method == 'POST':
+        comment_text = request.form['txt']
+        choice = request.form['choice']
+
+        if choice == "6":
+            pr0, pr1, pr2, pr3, pr4, pr5,
+            pa0, pa1, pa2, pa3, pa4, pa5 = pred(comment_text, choice)
+
+            return render_template('result.html', comment = comment_text, pr0 = pr0, pr1 = pr1, 
+            pr2 = pr2, pr3 = pr3, pr4 = pr4, pr5 = pr5, pa0 = pa0, pa1 = pa1, 
+            pa2 = pa2, pa3 = pa3, pa4 = pa4, pa5 = pa5)
+        else:
+            pr0, pa0 = pred(comment_text, choice)
+
+            return render_template('result.html', comment = comment_text, pr0 = pr0, pa0 = pa0)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/view")
+def view():
+    return render_template('view.html')
+
+if _name_ == '_main_':
 	app.run(debug=True)
