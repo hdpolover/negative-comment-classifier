dc98a1cd9267240b4a8e1eb9cac90b648c7abc2c
hdpolover <hendrapolover@gmail.com>
2019-06-16 23:23:24 +0700

    updated a few things

--- classifier.py
+++ classifier.py
@@ -1,65 +1,74 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("data\comment_data_RAW.csv")
+
+df_data = df[["comment_text", "toxic", "severe_toxic", "obscene",
+              "threat", "insult", "identity_hate"]]
+
+df_x = df_data["comment_text"]
+df_y = df_data[["toxic", "severe_toxic", "obscene",
+                "threat", "insult", "identity_hate"]]
+
+vect = TfidfVectorizer(stop_words="english", sublinear_tf=True)
+x = vect.fit_transform(df_x)
+
+# split data
+x_train, x_test, y_train, y_test = train_test_split(
+    x, df_y, test_size=0.2, random_state=42, shuffle=True)
+
+# make the classifier
+clf = LinearSVC()
+
+categories = ['toxic', 'severe_toxic', 'obscene',
+              'threat', 'insult', 'identity_hate']
+
+# list for text and predictions
+pr = []  # prediction result
+pa = []  # prediction accuracy
+
+
+def predAll(text):
+    # input text
+    data = [text]
+    vector = vect.transform(data).toarray()
+
+    for category in categories:
+        # fit train data
+        clf.fit(x_train, y_train[category])
+        prediction = clf.predict(vector)
+        # add each category prediction to the list
+        pr.append(prediction)
+        # accuracy
+        accuracy = clf.predict(x_test)
+        accuracy_result = accuracy_score(y_test[category], accuracy)
+        # add each category accuracy to the list
+        pa.append(accuracy_result)
+
+    # create a new list
+    retPr = []
+    # copy list to a new one
+    retPr = pr.copy()
+    # clear pr elements
+    pr.clear()
+
+    return retPr[0], retPr[1], retPr[2], retPr[3], retPr[4], retPr[5]
+
+
 def pred(text, choice):
-   import pandas as pd
-   from sklearn.feature_extraction.text import TfidfVectorizer
-   from sklearn.model_selection import train_test_split
-   from sklearn.svm import LinearSVC
-   from sklearn.metrics import accuracy_score
+    # input text
+    data = [text]
+    vector = vect.transform(data).toarray()
 
+    # fit train data
+    clf.fit(x_train, y_train[categories[choice]])
+    prediction = clf.predict(vector)
+    # accuracy
+    accuracy = clf.predict(x_test)
+    accuracy_result = accuracy_score(
+        y_test[categories[choice]], accuracy)
 
-   df = pd.read_csv("data\comment_data_RAW.csv")
-
-   df_data = df[["comment_text", "toxic", "severe_toxic", "obscene",
-      "threat", "insult", "identity_hate"]]
-
-   df_x = df_data["comment_text"]
-   df_y = df_data[["toxic", "severe_toxic", "obscene",
-      "threat", "insult", "identity_hate"]]
-
-   vect = TfidfVectorizer(stop_words="english", sublinear_tf=True)
-   x = vect.fit_transform(df_x)
-
-   #split data
-   x_train, x_test, y_train, y_test = train_test_split(x, df_y, test_size=0.2, random_state=42, shuffle=True)
-
-   clf = LinearSVC()
-
-   #input text
-   data = [text]
-   vector = vect.transform(data).toarray()
-
-   #list for text and predictions
-   prediction_result = []
-   prediction_accuracy = []
-
-   categories = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-
-   if choice == "6":
-      for category in categories:
-         #fit train data
-         clf.fit(x_train, y_train[category])
-         prediction = clf.predict(vector)
-         #add each category prediction to the list
-         prediction_result.append(prediction)
-         #accuracy
-         accuracy = clf.predict(x_test)      
-         accuracy_result = accuracy_score(y_test[category], accuracy)
-         #add each category accuracy to the list
-         prediction_accuracy.append(accuracy_result)
-
-      return prediction_result[0], prediction_result[1], prediction_result[2],
-      prediction_result[3], prediction_result[4], prediction_result[5],
-      prediction_accuracy[0], prediction_accuracy[1], prediction_accuracy[2],
-      prediction_accuracy[3], prediction_accuracy[4], prediction_accuracy[5]
-   else:
-      #fit train data
-      c = int(choice)
-      clf.fit(x_train, y_train[categories[c]])
-      prediction = clf.predict(vector)
-      #accuracy
-      accuracy = clf.predict(x_test)      
-      accuracy_result = accuracy_score(y_test[categories[c]], accuracy)
-
-      return prediction, accuracy_result
-   
-
-
+    return prediction, accuracy_result
--- flask_app.py
+++ flask_app.py
@@ -1,49 +1,61 @@
 from flask import Flask, render_template, url_for, request, redirect
 
-#import classifier.py
-from classifier import pred
+# import classifier.py
+from classifier import predAll, pred
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/clf")
 def clf():
     return render_template("classifier.html")
+
 
 @app.route("/predi", methods=['POST'])
 def predi():
     if request.method == 'POST':
         comment_text = request.form['txt']
-        choice = request.form['choice']
+        choice = int(request.form['choice'])
 
-        #category to print
-        cat = ['toxic', 'severe toxic', 'obscene', 'threat', 'insult', 'identity hate', 'negative']
-        #cast to int
-        index = int(choice)
+        # category to print
+        cat = ['toxic', 'severe toxic', 'obscene',
+               'threat', 'insult', 'identity hate', 'negative']
 
-        if choice == "6":
-            pr0, pr1, pr2, pr3, pr4, pr5,
-            pa0, pa1, pa2, pa3, pa4, pa5 = pred(comment_text, choice)
+        if choice == 6:
+            # pr0, pr1, pr2, pr3, pr4, pr5, pa0, pa1, pa2, pa3, pa4, pa5 = 
+            # predAll(comment_text)
+            pr0, pr1, pr2, pr3, pr4, pr5 = predAll(comment_text)
+            # store values to a list
+            allPr = [pr0, pr1, pr2, pr3, pr4, pr5]
 
-            return render_template('result.html', comment = comment_text, category = cat[index],
-            pr0 = pr0, pr1 = pr1, pr2 = pr2, pr3 = pr3, pr4 = pr4, pr5 = pr5, 
-            pa0 = pa0, pa1 = pa1, pa2 = pa2, pa3 = pa3, pa4 = pa4, pa5 = pa5)
+            res = 0
+            if 1 in allPr:
+                res = 1
+
+            return render_template('result.html', comment=comment_text,
+                                   category=cat[choice], res=res)
         else:
             pr0, pa0 = pred(comment_text, choice)
 
-            return render_template('result.html', comment = comment_text, category = cat[index],
-            pr0 = pr0, pa0 = pa0)
+            return render_template('result.html', comment=comment_text,
+                                   category=cat[choice],
+                                   res=pr0, a0=pa0)
+
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
+
 @app.route("/view")
 def view():
     return render_template('view.html')
 
+
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
